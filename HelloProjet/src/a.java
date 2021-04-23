1c8e55e728cb4ebc0ffbaa0f674e63c84353b154
zhw <532958440@qq.com>
2021-04-23 12:38:49 +0800

    a类

---
+++ HelloProjet/src/a.java
@@ -0,0 +1,12 @@
+package HelloProjet.src;
+
+/**
+ * @Author ZhangHangwei
+ * @Date 2021/4/23 12:36
+ * @Version 1.0
+ */
+public class a {
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
